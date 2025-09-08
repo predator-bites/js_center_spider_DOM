9741b8d5ae6180ef551e869e444e96c64b4f9043
DropPredator <ionchiccc@gmail.com>
2025-09-08 09:59:41 +0300

    ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,12 @@
 const spider = document.querySelector('.spider');
 const wall = document.querySelector('.wall');
 
-addEventListener('DOMContentLoaded', () => {
-  if (
-    wall.clientWidth !== null &&
-    typeof wall.clientWidth !== 'undefined' &&
-    spider.clientWidth !== null &&
-    typeof spider.clientWidth !== 'undefined' &&
-    wall.clientHeight !== null &&
-    typeof wall.clientHeight !== 'undefined' &&
-    spider.clientHeight !== null &&
-    typeof spider.clientHeight !== 'undefined'
-  ) {
-    wall.style.position = 'absolute';
+document.addEventListener('DOMContentLoaded', () => {
+  if (wall && spider) {
+    if (getComputedStyle(wall).position === 'static') {
+      wall.style.positiomn = 'relative';
+    }
+
     spider.style.position = 'absolute';
     spider.style.left = `${(wall.clientWidth - spider.clientWidth) / 2}px`;
     spider.style.top = `${(wall.clientHeight - spider.clientHeight) / 2}px`;
